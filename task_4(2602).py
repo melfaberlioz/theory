e9dbe44ee19af26f295ce96b4576336a3e86dcfe
melfaberlioz <sofi.horizon@gmail.com>
2023-03-03 08:13:27 +0200

    Initial Commit

--- task_4(2602).py
+++ task_4(2602).py
@@ -1,2 +1,9 @@
 # Визначити, скільки хвилин пройшло з початку
 # доби за введеним часом у форматі ГГ:ХХ
+
+time = input("Enter time in format HH:MM: ")
+colon_index = time.index(":")
+hours = int(time[:colon_index])
+minutes = int(time[colon_index+1:])
+
+print(f"here is {hours * 60 + minutes} minutes since the day has started.")
--- task_5(2602).py
+++ task_5(2602).py
@@ -1,2 +1,17 @@
 # Який час буде на годиннику (ГГ:ХХ),
 # якщо введено кількість хвилин, які пройшли з початку доби
+
+minutes_since_start = int(input("Enter minutes since the day has started: "))
+current_time = float(minutes_since_start / 60)
+# print(str(current_time))
+new_current_time = str(current_time)
+dot_index = new_current_time.index(".")
+hours_index = int(minutes_since_start[:dot_index])
+minutes_index = int(minutes_since_start[dot_index+1:])
+
+if hours_index < 10:
+    print(f"It's 0{hours_index} : {minutes_index}")
+elif hours_index <= 23:
+    print(f"It's {hours_index} : {minutes_index}")
+elif hours_index == 24:
+    print(f"It's 00 : {minutes_index}")
