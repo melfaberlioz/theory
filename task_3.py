f6eccd2ad115bc812a238d9c8d18dd2b6d17cf02
melfaberlioz <sofi.horizon@gmail.com>
2023-03-09 11:50:47 +0200

    Initial Commit

--- task_3.py
+++ task_3.py
@@ -1,26 +1,17 @@
 dollar = 40.25
-money = input("Enter value of money: ")
-
-
-# space_index = money.index(" ")
-# currency = money[space_index + 1:]
-# value = int(money[:space_index])
-#
-# if currency == "грн":
-#     print(f"{round(value / dollar, 2)} $")
-#     print(f"{value / dollar:6.2f} $")
-# else:
-#     print(f"{value * dollar} грн")
+money = input("Enter values of money: ")
 
 parts = money.split()
 currency = parts[1]
 value = int(parts[0])
 
+
 if currency == "грн":
-    #print(f"{round(value / dollar, 2)} $")
+    print(f"{round(value / dollar, 2)} $")
     print(f"{value / dollar:6.2f} $")
 else:
     print(f"{value * dollar} грн")
+
 
 
 
